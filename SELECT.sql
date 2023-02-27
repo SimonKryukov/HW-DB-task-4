3326f2ba4ee05a14c17fd165dc7fd36a886fd424
Simon Kryukov <kry-semen@mail.ru>
2023-02-27 17:16:10 +0300

    first corrections

--- SELECT.sql
+++ SELECT.sql
@@ -4,12 +4,9 @@ GROUP BY genre_id
 ORDER BY count(artist_id) DESC;
 
 --2) количество треков, вошедших в альбомы 2019-2020 годов;
-SELECT t.name, count(tracks_id) FROM tracks t
-WHERE album_id IN(
-	SELECT album_id FROM albums 
-	WHERE release_date BETWEEN '2019-01-01' AND '2020-12-31'
-)
-GROUP BY t.name;
+SELECT COUNT(t.tracks_id) FROM tracks t
+INNER JOIN albums alb ON t.album_id = alb.album_id
+WHERE alb.release_date BETWEEN '2019-01-01' AND '2020-12-31';
 
 --3) средняя продолжительность треков по каждому альбому;
 SELECT album_id, AVG(lenght) FROM tracks
@@ -18,19 +15,20 @@ ORDER BY AVG(lenght) DESC;
 
 --4) все исполнители, которые не выпустили альбомы в 2020 году;
 SELECT a.name FROM artist a
-JOIN albumsartists al ON a.artist_id = al.id
-JOIN albums alb ON al.id = alb.album_id
-WHERE release_date NOT BETWEEN '2020-01-01' AND '2020-12-31';
+WHERE a.artist_id NOT IN (
+    SELECT DISTINCT albumsartists.artist_id FROM albums alb
+    INNER JOIN albumsartists ON alb.album_id = albumsartists.album_id
+    WHERE alb.release_date >= '2020-01-01' AND alb.release_date <= '2020-12-31'
+);
 
 --5) названия сборников, в которых присутствует конкретный исполнитель (выберите сами);
---Почему то не находит все альбомы... Например "Muse" вбиваешь, не находит второй альбом и собственно сборник....
-SELECT DISTINCT m.name, t.name, a.name FROM mixtapes m 
-JOIN mixtapestracks mix ON m.mix_tapes_id = mix.mix_tapes_id
-JOIN tracks t ON mix.tracks_id = t.tracks_id
-JOIN albums alb ON t.album_id = alb.album_id
-JOIN albumsartists al ON alb.album_id = al.artist_id 
-JOIN artist a ON al.artist_id = a.artist_id 
-WHERE a.name = 'Madonna';
+SELECT mixtapes.name FROM mixtapes
+INNER JOIN mixtapestracks ON mixtapes.mix_tapes_id = mixtapestracks.mix_tapes_id
+INNER JOIN tracks ON mixtapestracks.tracks_id = tracks.tracks_id
+INNER JOIN albums ON tracks.album_id = albums.album_id
+INNER JOIN albumsartists ON albums.album_id = albumsartists.album_id
+INNER JOIN artist ON albumsartists.artist_id = artist.artist_id
+WHERE Artist.name = 'Muse';
 
 --6) название альбомов, в которых присутствуют исполнители более 1 жанра;
 SELECT al.name, COUNT(ar.artist_id) FROM albums al
@@ -60,14 +58,20 @@ JOIN tracks t ON al.album_id = t.album_id
 WHERE t.lenght = (SELECT MIN(lenght) FROM tracks);
 
 --9) название альбомов, содержащих наименьшее количество треков.
-SELECT DISTINCT al.name FROM albums al
-JOIN tracks t ON al.album_id = t.album_id
-WHERE t.album_id IN(
-	SELECT COUNT(album_id) FROM tracks
-	GROUP BY album_id
-	ORDER BY album_id 
+SELECT alb.name FROM albums alb
+INNER JOIN (
+    SELECT album_id, COUNT(*) AS tracks_count
+    FROM tracks
+    GROUP BY album_id
+) AS track_counts ON alb.album_id = track_counts.album_id
+WHERE track_counts.tracks_count = (
+    SELECT MIN(tracks_count)
+    FROM (
+        SELECT album_id, COUNT(*) AS tracks_count
+        FROM tracks
+        GROUP BY album_id
+    ) AS album_track_counts
 );
-
 
 
 
